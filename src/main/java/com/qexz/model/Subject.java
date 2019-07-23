80cb31e733cbcb4df84d1f22ab1ae086be7121e9
maishao1024 <541992791@qq.com>
2019-07-23 20:45:25 +0800

    初始版本

---
+++ src/main/java/com/qexz/model/Subject.java
@@ -0,0 +1,23 @@
+package com.qexz.model;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class Subject {
+
+    private int id;
+
+    private String name;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private int questionNum;
+
+    private String imgUrl;
+
+    private int state;
+
+}
